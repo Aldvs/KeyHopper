7ea8f605c7e5ed20653fd9272f1eb4bc57e218d0
aldvs <andrewgogol777@gmail.com>
2022-05-12 23:03:57 +0300

    1. Add DataManager 2. Add StorageManager 3. Setup TableViewControllers on main.storyboard 4. Add TableViewControllers

---
+++ KeyHopper/Services/DataManager.swift
@@ -0,0 +1,8 @@
+//
+//  DataManager.swift
+//  KeyHopper
+//
+//  Created by admin on 12.05.2022.
+//
+
+import Foundation
---
+++ KeyHopper/Services/StorageManager.swift
@@ -0,0 +1,8 @@
+//
+//  StorageManager.swift
+//  KeyHopper
+//
+//  Created by admin on 12.05.2022.
+//
+
+import Foundation
