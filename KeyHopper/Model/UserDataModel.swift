5a40b190fb02f843078fecf4bdf12482486f7087
aldvs <andrewgogol777@gmail.com>
2022-05-20 16:39:25 +0300

    1. Setup UserDataModel

---
+++ KeyHopper/Model/UserDataModel.swift
@@ -0,0 +1,25 @@
+//
+//  PasswordModel.swift
+//  KeyHopper
+//
+//  Created by admin on 12.05.2022.
+//
+
+import Foundation
+
+struct UserData {
+    
+    var nameOfAccount: String = ""
+    var password: String = ""
+    var hint: String = ""
+    
+    var login: String
+    var passwordForAuthorization: String
+    
+    func getUserData() -> UserData {
+        UserData(
+            login: "user",
+            passwordForAuthorization: "password")ç
+    }
+}
+
--- KeyHopper/ViewControllers/FormViewController.swift
+++ KeyHopper/ViewControllers/FormViewController.swift
@@ -9,14 +9,30 @@ import UIKit
 
 class FormViewController: UIViewController {
 
-    
+    //MARK: - IBOutlets
     @IBOutlet weak var loginTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
     
+    //MARK: - Private properties
+    private let userData = UserData.getUserData()
+    
+    // MARK: Navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let tabBarController = segue.destination as? UITabBarController else { return }
+        guard let viewControllers = tabBarController.viewControllers else { return }
+        
+        viewControllers.forEach {
+            if let welcomeVC = $0 as? MasterKeyViewController {
+                welcomeVC.user = user
+            } else if let navigationVC = $0 as? UINavigationController {
+                let userInfo = navigationVC.topViewController as! UserInfoViewController
+                userInfo.user = user
+            }
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
     @IBAction func logIn(_ sender: Any) {
